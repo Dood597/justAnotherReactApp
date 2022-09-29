c5bb212da2ced7934cee386143ed07df88d4b376
Dood597 <66718486+Dood597@users.noreply.github.com>
2022-09-29 18:58:08 +0200

    React app

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
+
+  const getCat = () =>{
+    fetch("https://aws.random.cat/meow")
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(data)
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={getCat}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
